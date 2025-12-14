6d31be56f1930adbcf5940bd6dea1b966605f3f7
Santcar05 <castrogarzonsantiago@gmail.com>
2025-12-14 12:51:06 -0500

    SideBar Creado visualmente

---
+++ codequest-frontend/src/app/ui/pages/dashboardpage/dashboardpage.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Sidebar } from '../../components/sidebar/sidebar';
+
+@Component({
+  selector: 'app-dashboardpage',
+  imports: [Sidebar],
+  templateUrl: './dashboardpage.html',
+  styleUrl: './dashboardpage.css',
+})
+export class Dashboardpage {}
