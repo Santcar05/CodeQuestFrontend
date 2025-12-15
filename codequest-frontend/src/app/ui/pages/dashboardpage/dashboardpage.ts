17961d948ea0c82ad665809fca480001cc515105
Santcar05 <castrogarzonsantiago@gmail.com>
2025-12-14 23:56:56 -0500

    Logros diarios añadido a Dashboard

--- codequest-frontend/src/app/ui/pages/dashboardpage/dashboardpage.ts
+++ codequest-frontend/src/app/ui/pages/dashboardpage/dashboardpage.ts
@@ -6,14 +6,19 @@ import { RecommendedChallenge } from '../../components/recommended-challenge/rec
 import { MinimalizedCourseCard } from '../../components/minimalized-course-card/minimalized-course-card';
 import { ProgressBar } from '../../shared/progress-bar/progress-bar';
 import { CommonModule } from '@angular/common';
+import { DailyQuestCard } from '../../components/daily-quest-card/daily-quest-card';
 
 @Component({
   selector: 'app-dashboardpage',
-  imports: [RecommendedChallenge, MinimalizedCourseCard, ProgressBar, CommonModule],
+  imports: [RecommendedChallenge, MinimalizedCourseCard, ProgressBar, CommonModule, DailyQuestCard],
   templateUrl: './dashboardpage.html',
   styleUrl: './dashboardpage.css',
 })
 export class Dashboardpage {
+  // Pagination
+  page = 1;
+  pageSize = 4;
+
   level = 5;
   streak = 14;
 
@@ -24,5 +29,30 @@ export class Dashboardpage {
 
   get xpPercent(): number {
     return (this.currentXP / this.maxXP) * 100;
+  }
+
+  /*
+  Pagination methods
+   */
+  get totalPages(): number {
+    return Math.ceil(this.courses.length / this.pageSize);
+  }
+
+  get paginatedCourses() {
+    const start = (this.page - 1) * this.pageSize;
+    const end = start + this.pageSize;
+    return this.courses.slice(start, end);
+  }
+
+  nextPage() {
+    if (this.page < this.totalPages) {
+      this.page++;
+    }
+  }
+
+  prevPage() {
+    if (this.page > 1) {
+      this.page--;
+    }
   }
 }
